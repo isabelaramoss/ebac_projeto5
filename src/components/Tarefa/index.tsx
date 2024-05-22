af6e53462bdcc5d0246ad95779fbd3af79fbc153
Isabela Ramos <isamrteixeira@gmail.com>
2024-05-21 22:48:00 -0300

    useState botão editando

--- src/components/Tarefa/index.tsx
+++ src/components/Tarefa/index.tsx
@@ -1,16 +1,31 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-const Tarefa = () => (
-  <S.Card>
-    <S.Titulo>Nome da tarefa</S.Titulo>
-    <S.Tag>importante</S.Tag>
-    <S.Tag>pendente</S.Tag>
-    <S.Descricao />
-    <S.BarraAcoes>
-      <S.Botao>Editar</S.Botao>
-      <S.Botao>Remover</S.Botao>
-    </S.BarraAcoes>
-  </S.Card>
-)
+const Tarefa = () => {
+  //useState
+  //Se estiver editando não mostrar os botões editar e remover e sim salvar e cancelar.
+  const [estaEditando, setEstaEditando] = useState(false)
+  return (
+    <S.Card>
+      <S.Titulo>Nome da tarefa</S.Titulo>
+      <S.Tag>importante</S.Tag>
+      <S.Tag>pendente</S.Tag>
+      <S.Descricao />
+      <S.BarraAcoes>
+        {estaEditando ? (
+          <>
+            <S.Botao>Salvar</S.Botao>
+            <S.Botao onClick={() => setEstaEditando(false)}>Cancelar</S.Botao>
+          </>
+        ) : (
+          <>
+            <S.Botao onClick={() => setEstaEditando(true)}>Editar</S.Botao>
+            <S.Botao>Remover</S.Botao>
+          </>
+        )}
+      </S.BarraAcoes>
+    </S.Card>
+  )
+}
 
 export default Tarefa
